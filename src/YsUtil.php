68495ac11a4ad393825a7822350106366c58d85d
Yee Siang <yee.siang@mymagic.my>
2017-06-09 17:33:50 +0800

    added function to generate UUID

--- src/YsUtil.php
+++ src/YsUtil.php
@@ -9,6 +9,16 @@ class YsUtil
         return $bool;
     }
 
+	public function generateUUID()
+	{
+		if (function_exists('com_create_guid') === true)
+		{
+			return trim(com_create_guid(), '{}');
+		}
+
+		return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+	}
+	
 	/*
 		$params['smtpHost'] 
 		$params['smtpSecure'] 
