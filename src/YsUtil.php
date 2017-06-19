8c9044f02fc46004c46d0552511032ecec885343
Yee Siang <yee.siang@mymagic.my>
2017-06-19 16:19:16 +0800

    make sure generated uuid are always in lowercase

--- src/YsUtil.php
+++ src/YsUtil.php
@@ -16,7 +16,7 @@ class YsUtil
 			return trim(com_create_guid(), '{}');
 		}
 
-		return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+		return strtolower(sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535)));
 	}
 	
 	/*
