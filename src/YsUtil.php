0b5fd2856b569e140e1de6da5e50c0fe48778786
Yee Siang <yee.siang@mymagic.my>
2018-04-19 11:58:52 +0800

    added function strPutCsv, convert array to csv string

--- src/YsUtil.php
+++ src/YsUtil.php
@@ -9,6 +9,22 @@ class YsUtil
         return $bool;
     }
 
+	function strPutCsv($input, $delimiter = ',', $enclosure = '"')
+    {
+        // Open a memory "file" for read/write...
+        $fp = fopen('php://temp', 'r+');
+        // ... write the $input array to the "file" using fputcsv()...
+        fputcsv($fp, $input, $delimiter, $enclosure);
+        // ... rewind the "file" so we can read what we just wrote...
+        rewind($fp);
+        // ... read the entire line into a variable...
+        $data = fread($fp, 1048576);
+        // ... close the "file"...
+        fclose($fp);
+        // ... and return the $data to the caller, with the trailing newline from fgets() removed.
+        return rtrim($data, "\n");
+	}
+	
 	public function generateUUID()
 	{
 		if (function_exists('com_create_guid') === true)
